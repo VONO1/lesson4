c8730053b6d0ba31322b54901888f6864186ce71
VONO1 <Rb9427377>
2020-05-06 23:22:08 +0300

    222

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from random import randint
 
 spisok = ['Паша','Маша', 'Галя','Инна','Марина',"Никита",'Петя','Витя','Миша','Лера','Таня','Аня','Соня','Полина']
+
+#функция создания списка
 def FFF (sp, num):
     newsp= []
     for i in range(num):
@@ -9,4 +11,22 @@ def FFF (sp, num):
         newsp.append(sp[rn])
     return newsp
 
-print(FFF(spisok,20))
+#создаём список
+names_list=FFF(spisok,20)
+print(names_list)
+
+#сортировка:
+names_counter = {}
+for word in names_list:
+     if word in names_counter:
+         names_counter[word] += 1
+     else:
+         names_counter[word] = 1
+
+popular_words = sorted(names_counter, key = names_counter.get, reverse = True)
+
+top_3 = popular_words[:1]
+print(top_3)
+
+
+#print(FFF(spisok,20))
